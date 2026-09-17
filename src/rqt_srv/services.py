9413e9e42f5d0282cfd5669c11de19e05554c1c4
Alejandro Hernández Cordero <ahcorde@gmail.com>
2026-09-17 19:09:38 +0200

    Added missing deps and python3 improvements (#20)
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- src/rqt_srv/services.py
+++ src/rqt_srv/services.py
@@ -43,14 +43,14 @@ class Services(Plugin):
     # used to create tree_view elements will work.
 
     def __init__(self, context):
-        super(Services, self).__init__(context)
+        super().__init__(context)
         self.setObjectName('services')
         self._widget = MessagesWidget(message_helpers.SRV_MODE)
         self._widget.setWindowTitle('Service Type Browser')
         self._widget.type_label.setText('Service:')
         if context.serial_number() > 1:
             self._widget.setWindowTitle(
-                self._widget.windowTitle() + (' (%d)' % context.serial_number()))
+                self._widget.windowTitle() + (f' ({context.serial_number()})'))
         context.add_widget(self._widget)
 
     def shutdown_plugin(self):
